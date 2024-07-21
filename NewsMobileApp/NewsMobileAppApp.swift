e2d0477f31bdc1e0a131d8237dfc63896680210c
bharaa <bharalfhani@gmail.com>
2024-07-21 23:44:30 +0700

    Initial Commit

---
+++ NewsMobileApp/NewsMobileAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  NewsMobileAppApp.swift
+//  NewsMobileApp
+//
+//  Created by Bhara Alfhaniawan on 21/07/24.
+//
+
+import SwiftUI
+
+@main
+struct NewsMobileAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
